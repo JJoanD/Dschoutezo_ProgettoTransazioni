d07b6f88ebf3423fad8d763142929277ca28f225
unknown <dschoutezo-kenfack.joan@itismeucci.com>
2025-12-19 16:51:46 +0100

    fatta classe Account e main

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,5 +1,125 @@
 //
-// Created by hp on 19/12/2025.
+// Created by hp on 11/12/2025.
 //
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
 #include "Transaction.h"
+#include <sstream>
+#include <stdexcept>
+#include <iostream>
+
+// Costruttore di default
+Transaction::Transaction()
+    : type(TransactionType::Income),
+      amount(0.0),
+      date(std::chrono::year{2026}/1/1),
+      description(""),
+      category("") {}
+
+// Costruttore completo
+Transaction::Transaction(TransactionType type,double amount,const std::chrono::year_month_day& date,const std::string& description,const std::string& category)
+        : type(type),
+          amount(amount),
+          date(date),
+          description(description),
+          category(category) {}
+
+// Getter
+TransactionType Transaction::getType() const { return type; }
+double Transaction::getAmount() const { return amount; }
+const std::chrono::year_month_day& Transaction::getDate() const { return date; }
+const std::string& Transaction::getDescription() const { return description; }
+const std::string& Transaction::getCategory() const { return category; }
+
+// Setter
+void Transaction::setType(TransactionType t) { type = t; }
+void Transaction::setAmount(double a) { amount = a; }
+void Transaction::setDate(const std::chrono::year_month_day& d) { date = d; }
+void Transaction::setDescription(const std::string& desc) { description = desc; }
+void Transaction::setCategory(const std::string& cat) { category = cat; }
+
+//altro
+void Transaction::toString(bool colored) const
+{
+    const char* color = "";
+    const char* reset = "";
+
+    if (colored) {
+        color = (type == TransactionType::Income ? GREEN : RED);
+        reset = RESET;
+    }
+
+    std::cout
+      << "- "
+      << (type == TransactionType::Income ? "[ENTRATA] " : "[USCITA] ")
+      << description
+      << " | " << color << " Importo: " << (type == TransactionType::Income? "+" : "-" )<< amount << reset
+      << " | Data: " << this->getDate().year() << "-" << this->getDate().month() << "-" << this->getDate().day()
+      << " | Categoria: " << category << "\n"
+      ;
+}
+
+std::string Transaction::serialize() const {
+    std::ostringstream newLine;
+
+    // Tipo: "INCOME" o "EXPENSE"
+    std::string typeTrans = (type == TransactionType::Income ? "INCOME" : "EXPENSE");
+
+    // Formato: tipo;YYYY-MM-DD;amount;description;category
+    newLine << typeTrans << ";"
+        << this->getDate().year() << "-" << this->getDate().month() << "-" << this->getDate().day() << ";"
+        << amount << ";"
+        << description << ";"
+        << category;
+
+    return newLine.str();
+}
+
+
+Transaction Transaction::deserialize(const std::string& line) {
+
+    std::stringstream ss(line);   // flusso sulla riga
+    std::string part;             // campo temporaneo
+
+    // ---------- TIPO ----------
+    std::getline(ss, part, ';');
+    TransactionType tType;
+
+    if (part == "INCOME") {
+        tType = TransactionType::Income;
+    } else if (part == "EXPENSE") {
+        tType = TransactionType::Expense;
+    } else {
+        throw std::runtime_error("Tipo di transazione non valido");
+    }
+
+    // ---------- DATA ----------
+    std::getline(ss, part, ';');  // YYYY-MM-DD
+    int y, m, d;
+    char dash1, dash2;
+
+    std::stringstream dateStream(part);
+    dateStream >> y >> dash1 >> m >> dash2 >> d;
+
+    std::chrono::year_month_day ymd{
+        std::chrono::year{y},
+        std::chrono::month{static_cast<unsigned>(m)},
+        std::chrono::day{static_cast<unsigned>(d)}
+    };
+
+    // ---------- IMPORTO ----------
+    std::getline(ss, part, ';');
+    double amount = std::stod(part);
+
+    // ---------- DESCRIZIONE ----------
+    std::string description;
+    std::getline(ss, description, ';');
+
+    // ---------- CATEGORIA ----------
+    std::string category;
+    std::getline(ss, category);   // fino a fine riga
+
+    return Transaction(tType, amount, ymd, description, category);
+}
--- Transaction.h
+++ Transaction.h
@@ -1,16 +1,66 @@
 //
-// Created by hp on 19/12/2025.
+// Created by hp on 11/12/2025.
 //
 
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
+#include <string>
+#include <chrono>
+
+enum class TransactionType {
+    Income,
+    Expense
+}; // si utilizza un enum class per differenziare i diversi tipi di transazione
 
 
 class Transaction {
+public:
+    Transaction();
 
+    Transaction(TransactionType type,
+                double amount,
+                const std::chrono::year_month_day &date,
+                const std::string &description,
+                const std::string &category = "");
+
+    // Getter
+    TransactionType getType() const;
+
+    double getAmount() const;
+
+    const std::chrono::year_month_day &getDate() const;
+
+    const std::string &getDescription() const;
+
+    const std::string &getCategory() const;
+
+    // Setter
+    void setType(TransactionType type);
+
+    void setAmount(double amount);
+
+    void setDate(const std::chrono::year_month_day &date);
+
+    void setDescription(const std::string &description);
+
+    void setCategory(const std::string &category);
+
+    void toString(bool colored = false) const;
+
+    // Converte la transazione in una riga di testo per il file
+    std::string serialize() const;
+
+    // Crea una transazione a partire da una riga di testo del file
+    static Transaction deserialize(const std::string& line);
+
+private:
+    TransactionType type;
+    double amount;
+    std::chrono::year_month_day date;
+    std::string description;
+    std::string category;
 };
-
 
 
 #endif //TRANSACTION_H
